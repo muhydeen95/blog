b69d64814e46cc15f321f3290433811520ac3c6e
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2021-03-19 22:43:36 +0100

    port changed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 //connect to mongoDB
 const dbURI = 'mongodb+srv://netninja:test1234@nodetuts.vkko6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(3000))
+    .then((result) => app.listen(process.env.PORT ||3000))
     .catch((err) => console.log(err));
 //Register view engine
 app.set('view engine', 'ejs')
